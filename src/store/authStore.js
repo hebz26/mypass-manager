669bcfbb1588fd295b2e3d17e62105f854c7f960
Heba Sayed <hebasayed1603@gmail.com>
2024-11-22 16:45:06 -0500

    singleton

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,7 +1,10 @@
 import { create } from "zustand";
 
+// Creating a Zustand store. This is a singleton store for global state management.
+// Since Zustand uses hooks, there is only one instance of this store, shared across the application, which is why it's a singleton.
+
 const useAuthStore = create((set) => ({
-  user: JSON.parse(localStorage.getItem("user-info") || "null"), // Default to null if no data is found
+  user: JSON.parse(localStorage.getItem("user-info") || "null"),
   login: (user) => set({ user }),
   logout: () => set({ user: null }),
   setUser: (user) => set({ user }),
