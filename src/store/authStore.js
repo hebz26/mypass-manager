8bc68d9f3390a954175f7caf4420560fd77b099c
Heba Sayed <hebasayed1603@gmail.com>
2024-11-24 19:12:46 -0500

    fixed sudden error with auth

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -4,7 +4,15 @@ import { create } from "zustand";
 // Since Zustand uses hooks, there is only one instance of this store, shared across the application, which is why it's a singleton.
 
 const useAuthStore = create((set) => ({
-  user: JSON.parse(localStorage.getItem("user-info") || "null"),
+  user: (() => {
+    const storedUser = localStorage.getItem("user-info");
+    try {
+      return storedUser ? JSON.parse(storedUser) : null;
+    } catch (error) {
+      console.error("Error parsing user-info from localStorage:", error);
+      return null; // Fallback if the stored data is invalid or can't be parsed
+    }
+  })(),
   login: (user) => set({ user }),
   logout: () => set({ user: null }),
   setUser: (user) => set({ user }),
